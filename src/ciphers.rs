8abff85cf9e8a280a71aeb8a304ba5fc7371e1af
[Error_27] <error27dev@gmail.com>
2024-05-04 21:42:49 -0400

    caesar cipher now accounts for special chars and overflows

--- src/ciphers.rs
+++ src/ciphers.rs
@@ -2,6 +2,17 @@ pub fn encode_caesar(input: String, shift: usize) -> String {
     input
     .chars()
     .map(|c| {
+        if !c.is_alphabetic() {
+            return Some(c)
+        }
+        if c.is_lowercase() && c as u32 + shift as u32 > 'z' as u32 {
+            let remainder = shift as u32 - ('z' as u32 - c as u32);
+            return char::from_u32('a' as u32 - 1 + remainder)
+        }
+        if c.is_uppercase() && c as u32 + shift as u32 > 'Z' as u32 {
+            let remainder = shift as u32 - ('Z' as u32 - c as u32);
+            return char::from_u32('A' as u32 - 1 + remainder)
+        }
         char::from_u32(c as u32 + shift as u32)
     })
     .flatten()
@@ -9,11 +20,5 @@ pub fn encode_caesar(input: String, shift: usize) -> String {
 }
 
 pub fn decode_caesar(input: String, shift: usize) -> String {
-    input
-    .chars()
-    .map(|c| {
-        char::from_u32(c as u32 - shift as u32)
-    })
-    .flatten()
-    .collect()
+    encode_caesar(input, 26 - shift)
 }
