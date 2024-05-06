3d7e5db30c6d22e2691e1ce52fc18a39f7b94178
[Error_27] <error27dev@gmail.com>
2024-05-05 21:36:18 -0400

    add doc comments

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,33 @@
 use clap::{Parser, Subcommand};
 mod ciphers;
 
+/// Terminal program to encrypt and decrypt text strings!
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
 struct Args {
+    /// Cipher to use
     #[command(subcommand)]
     cipher: Cipher,
 
+    /// Decipher an input
     #[clap(long, short, action)]
     decipher: bool,
 }
 
 #[derive(Subcommand, Debug, Clone)]
 enum Cipher {
+    /// Use a Caesar cipher
     Caesar {
+        /// Amount to shift the alphabet
         shift: usize,
+        /// Text to encrypt
         input: String
     },
+    /// Use a Vigenere cipher
     Vigenere {
+        /// Keyword to encrypt by (only alphabetic characters are counted)
         key: String,
+        /// Text to encrypt
         input: String
     }
 }
